a53f3da4f1dd956cb480b0e51f2b0d1989d6c394
scast <scast@DESKTOP-FJHIK6H>
2020-10-21 17:00:58 +0200

    Fith update

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -7,18 +7,14 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        int data [] = {1,5,2,6,9,8,5,36,9,4,1,5,7,77,85};
-        Map<Integer,Integer> histogram = new HashMap<Integer, Integer>();
+        Integer data [] = {1,5,2,6,9,8,5,36,9,4,1,5,7,77,85};
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
         
-        for (int key:data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-        }
-        Iterator<Map.Entry<Integer, Integer>> entries = histogram.entrySet().iterator();
+        Iterator<Map.Entry<Integer, Integer>> entries = histogr.entrySet().iterator();
         while (entries.hasNext()) {
             Map.Entry<Integer, Integer> entry = entries.next();
             System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
         }
-        
     }
-    
 }
