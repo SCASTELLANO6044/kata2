f0b63eebbe34d2892ee2b01cad8fbfce41c2e570
scast <scast@DESKTOP-FJHIK6H>
2020-10-21 15:31:28 +0200

    First update

---
+++ src/kata2/Main.java
@@ -0,0 +1,14 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        
+    }
+    
+}
